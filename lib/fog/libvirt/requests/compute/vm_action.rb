04ea0cd6d2f9e43e1da89f446493bc08d115a43f
Delf Neumärker <neumaerker@byonoy.com>
2021-07-30 16:44:52 +0200

    Add flags option to server.destroy

--- lib/fog/libvirt/requests/compute/vm_action.rb
+++ lib/fog/libvirt/requests/compute/vm_action.rb
@@ -2,15 +2,15 @@ module Fog
   module Libvirt
     class Compute
       class Real
-        def vm_action(uuid, action)
+        def vm_action(uuid, action, *params)
           domain = client.lookup_domain_by_uuid(uuid)
-          domain.send(action)
+          domain.send(action, *params)
           true
         end
       end
 
       class Mock
-        def vm_action(uuid, action)
+        def vm_action(uuid, action, *params)
           true
         end
       end
