fog/fog-libvirt

commit 085a7c89596e989921dccd33dd954afbb37df27a

Add flags option to server.destroy

lib/fog/libvirt/models/compute/server.rb
@@ -90,9 +90,13 @@ module Fog
           volumes.first.path if volumes and volumes.first
         end
 
-        def destroy(options={ :destroy_volumes => false})
+        def destroy(options={ :destroy_volumes => false, :flags => 0 })
           poweroff unless stopped?
-          service.vm_action(uuid, :undefine)
+          if options[:flags].zero?
+            service.vm_action(uuid, :undefine)
+          else
+            service.vm_action(uuid, :undefine, options[:flags])
+          end
           volumes.each { |vol| vol.destroy } if options[:destroy_volumes]
           true
         end

.rubocop.yml
@@ -18,3 +18,6 @@ SignalException:
   Description: 'Do not enforce use of fail when raising exceptions.'
   # Valid values are: semantic, only_raise and only_fail
   EnforcedStyle: only_raise
+
+Metrics/ClassLength:
+  Enabled: false

lib/fog/libvirt/requests/compute/vm_action.rb
@@ -2,15 +2,15 @@ module Fog
   module Libvirt
     class Compute
       class Real
-        def vm_action(uuid, action)
+        def vm_action(uuid, action, *params)
           domain = client.lookup_domain_by_uuid(uuid)
-          domain.send(action)
+          domain.send(action, *params)
           true
         end
       end
 
       class Mock
-        def vm_action(uuid, action)
+        def vm_action(uuid, action, *params)
           true
         end
       end

commit 4cd57295ec47f452877cb5d4bc82293ec1f4db97

Deal with no flags passed to Server#destroy
In 085a7c89596e989921dccd33dd954afbb37df27a it became possible to pass
flags to Server#destroy but if the caller didn't pass flags in opts it
failed because it was essentially nil.zero? which isn't defined.

Fixes: 085a7c89596e989921dccd33dd954afbb37df27a

lib/fog/libvirt/models/compute/server.rb
@@ -92,7 +92,7 @@ module Fog
 
         def destroy(options={ :destroy_volumes => false, :flags => 0 })
           poweroff unless stopped?
-          if options[:flags].zero?
+          if options.fetch(:flags, 0).zero?
             service.vm_action(uuid, :undefine)
           else
             service.vm_action(uuid, :undefine, options[:flags])