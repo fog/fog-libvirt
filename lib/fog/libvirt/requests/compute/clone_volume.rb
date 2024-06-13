1056bf0f37e37f67b62767ee02644736f60dfcb4
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2024-06-13 18:45:55 +0200

    Fix coding style

--- lib/fog/libvirt/requests/compute/clone_volume.rb
+++ lib/fog/libvirt/requests/compute/clone_volume.rb
@@ -2,7 +2,7 @@ module Fog
   module Libvirt
     class Compute
       class Real
-        def clone_volume (pool_name, xml, name)
+        def clone_volume(pool_name, xml, name)
           vol = client.lookup_storage_pool_by_name(pool_name).lookup_volume_by_name(name)
           client.lookup_storage_pool_by_name(pool_name).create_vol_xml_from(xml, vol)
         end
