fog/fog-libvirt

commit 21d1bf827c03ae79e1f5a10118c2b41fb80e0be7

Undefine NVRAM on vm destruction
VMs with UEFI store UEFI variables in OVMF files. This flag tells
Libvirt to undefine it, if it exists. This was introduced in libvirt
1.2.6, which can be found on very old EL7 systems, but those are no
longer supported. EL 7.9 (only supported EL7 version) ships libvirt 4.5.

lib/fog/libvirt/models/compute/server.rb
@@ -90,9 +90,9 @@ module Fog
           volumes.first.path if volumes and volumes.first
         end
 
-        def destroy(options={ :destroy_volumes => false, :flags => 0 })
+        def destroy(options={ :destroy_volumes => false, :flags => ::Libvirt::Domain::UNDEFINE_NVRAM })
           poweroff unless stopped?
-          if options.fetch(:flags, 0).zero?
+          if options.fetch(:flags, ::Libvirt::Domain::UNDEFINE_NVRAM).zero?
             service.vm_action(uuid, :undefine)
           else
             service.vm_action(uuid, :undefine, options[:flags])

commit 922f0a5b3c696f2ec5afdfea6133dd2b11c2ee1b

Correctly pass UNDEFINE_NVRAM when flags isn't pased
If destroy() is called with options that doesn't contain a flags value
then it should still default to UNDEFINE_NVRAM. That wasn't happening.

Fixes: 21d1bf827c03 ("Undefine NVRAM on vm destruction")

lib/fog/libvirt/models/compute/server.rb
@@ -92,10 +92,11 @@ module Fog
 
         def destroy(options={ :destroy_volumes => false, :flags => ::Libvirt::Domain::UNDEFINE_NVRAM })
           poweroff unless stopped?
-          if options.fetch(:flags, ::Libvirt::Domain::UNDEFINE_NVRAM).zero?
+          flags = options.fetch(:flags, ::Libvirt::Domain::UNDEFINE_NVRAM)
+          if flags.zero?
             service.vm_action(uuid, :undefine)
           else
-            service.vm_action(uuid, :undefine, options[:flags])
+            service.vm_action(uuid, :undefine, flags)
           end
           volumes.each { |vol| vol.destroy } if options[:destroy_volumes]
           true