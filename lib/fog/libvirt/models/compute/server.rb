fog/fog-libvirt

commit 7a3a46b48fe7387585eead28396b2ad8d29d1567

Prefer CPU host pass through over explicit models

lib/fog/libvirt/models/compute/server.rb
@@ -295,12 +295,16 @@ module Fog
               end
 
               unless cpu.empty?
-                if cpu[:mode]
-                  xml.cpu(:mode => cpu[:mode])
-                else
+                if cpu.dig(:model, :name)
                   xml.cpu do
                     xml.model(cpu.dig(:model, :name), :fallback => cpu.dig(:model, :fallback) || "allow")
                   end
+                else
+                  xml.cpu(
+                    :mode => cpu.dig(:model, :name) || "host-passthrough",
+                    :check => cpu.fetch(:check, "none"),
+                    :migratable => cpu.fetch(:migratable, "on")
+                  )
                 end
               end
 

commit 11f3821777becf14b3bb3c14122f8cb5af55a8c7

Allow setting cpu mode again
The value for `cpu.dig(:model, :name)` is always `nil` since that's the
condition in the `if` statement. It was intended to still respect the
cpu mode option passed in.

Fixes: 7a3a46b48fe7 ("Prefer CPU host pass through over explicit models")

lib/fog/libvirt/models/compute/server.rb
@@ -307,7 +307,7 @@ module Fog
                   end
                 else
                   xml.cpu(
-                    :mode => cpu.dig(:model, :name) || "host-passthrough",
+                    :mode => cpu.fetch(:mode, "host-passthrough"),
                     :check => cpu.fetch(:check, "none"),
                     :migratable => cpu.fetch(:migratable, "on")
                   )