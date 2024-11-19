fog/fog-libvirt

commit 801ce47ed9fe1295f2753eafabdf4e97021767f7

Use test:///default in tests
Rather than mocking, this uses the test connection to use real libvirt
objects. This brings compatibility with 0.8.3.

It only keeps mocking for dhcp_leases which isn't implemented in the
test connection. The UUIDs are modified to match.

lib/fog/libvirt/compute.rb
@@ -49,34 +49,12 @@ module Fog
 
       module Shared
         include Fog::Libvirt::Util
-      end
 
-      class Mock
-        include Shared
-        def initialize(options={})
-          # libvirt is part of the gem => ruby-libvirt
-          require 'libvirt'
-        end
-
-        private
-
-        def client
-          return @client if defined?(@client)
-        end
-
-        #read mocks xml
-        def read_xml(file_name)
-          file_path = File.join(File.dirname(__FILE__),"requests","compute","mock_files",file_name)
-          File.read(file_path)
-        end
-      end
-
-      class Real
-        include Shared
         attr_reader :client
         attr_reader :uri
 
         def initialize(options={})
+          super()
           @uri = ::Fog::Libvirt::Util::URI.new(enhance_uri(options[:libvirt_uri]))
 
           # libvirt is part of the gem => ruby-libvirt
@@ -131,6 +109,14 @@ module Fog
           end
           uri+append
         end
+      end
+
+      class Real
+        include Shared
+      end
+
+      class Mock
+        include Shared
       end
     end
   end

Gemfile
@@ -6,7 +6,4 @@ group :development, :test do
   gem "octokit", :require => false
 end
 
-# 0.8.3 breaks our tests
-gem "ruby-libvirt", ">= 0.7.0", "< 0.8.3"
-
 gemspec

lib/fog/libvirt/requests/compute/clone_volume.rb
@@ -1,17 +1,19 @@
 module Fog
   module Libvirt
     class Compute
-      class Real
+      module Shared
         def clone_volume(pool_name, xml, name)
           vol = client.lookup_storage_pool_by_name(pool_name).lookup_volume_by_name(name)
           client.lookup_storage_pool_by_name(pool_name).create_vol_xml_from(xml, vol)
         end
       end
 
+      class Real
+        include Shared
+      end
+
       class Mock
-        def clone_volume(pool_name, xml, name)
-          Fog::Libvirt::Compute::Volume.new({:pool_name => pool_name, :xml => xml})
-        end
+        include Shared
       end
     end
   end

lib/fog/libvirt/requests/compute/create_domain.rb
@@ -1,16 +1,18 @@
 module Fog
   module Libvirt
     class Compute
-      class Real
+      module Shared
         def create_domain(xml)
           client.create_domain_xml(xml)
         end
       end
 
+      class Real
+        include Shared
+      end
+
       class Mock
-        def create_domain(xml)
-          ::Libvirt::Domain.new()
-        end
+        include Shared
       end
     end
   end

lib/fog/libvirt/requests/compute/create_volume.rb
@@ -1,15 +1,18 @@
 module Fog
   module Libvirt
     class Compute
-      class Real
+      module Shared
         def create_volume(pool_name, xml)
           client.lookup_storage_pool_by_name(pool_name).create_vol_xml(xml)
         end
       end
 
+      class Real
+        include Shared
+      end
+
       class Mock
-        def create_volume(pool_name, xml)
-        end
+        include Shared
       end
     end
   end

lib/fog/libvirt/requests/compute/define_domain.rb
@@ -1,16 +1,18 @@
 module Fog
   module Libvirt
     class Compute
-      class Real
+      module Shared
         def define_domain(xml)
           client.define_domain_xml(xml)
         end
       end
 
+      class Real
+        include Shared
+      end
+
       class Mock
-        def define_domain(xml)
-          ::Libvirt::Domain.new()
-        end
+        include Shared
       end
     end
   end

lib/fog/libvirt/requests/compute/define_pool.rb
@@ -1,15 +1,18 @@
 module Fog
   module Libvirt
     class Compute
-      class Real
+      module Shared
         def define_pool(xml)
           client.define_storage_pool_xml(xml)
         end
       end
 
+      class Real
+        include Shared
+      end
+
       class Mock
-        def define_pool(xml)
-        end
+        include Shared
       end
     end
   end

lib/fog/libvirt/requests/compute/destroy_interface.rb
@@ -1,17 +1,19 @@
 module Fog
   module Libvirt
     class Compute
-      class Real
+      module Shared
         #shutdown the interface
         def destroy_interface(uuid)
           client.lookup_interface_by_uuid(uuid).destroy
         end
       end
 
+      class Real
+        include Shared
+      end
+
       class Mock
-        def destroy_interface(uuid)
-          true
-        end
+        include Shared
       end
     end
   end

commit 47cfacef6541e40774cde6c0592429c97000511a

Force test:// URI to be used in mocked connections
Prior to 801ce47ed9fe1295f2753eafabdf4e97021767f7 any URL could be used
when mocking and it had preconfigured responses, but this regressed and
could cause real system connections to go through.

This forces the URL to use the test:// scheme which still allows the use
of test:///path/to/host/definitions as documented.

Fixes: 801ce47ed9fe ("Use test:///default in tests")
Reference: https://libvirt.org/uri.html#test-test-uris

lib/fog/libvirt/compute.rb
@@ -117,6 +117,12 @@ module Fog
 
       class Mock
         include Shared
+
+        def enhance_uri(uri)
+          uri = 'test:///default' unless ::URI.parse(uri).scheme == 'test'
+
+          super(uri)
+        end
       end
     end
   end