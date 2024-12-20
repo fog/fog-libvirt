fog/fog-libvirt

commit 6db6f8089418599044be39373f3baf17402b5cca

Replace server XML with Nokogiri::XML::Builder
This replaces the ERB template with a real XML builder. This makes it
much easier to maintain.

lib/fog/libvirt/models/compute/server.rb
@@ -267,8 +267,165 @@ module Fog
           "#{name}-cloud-init.iso"
         end
 
+        # rubocop:disable Metrics
+        def to_xml
+          builder = Nokogiri::XML::Builder.new do |xml|
+            xml.domain(:type => domain_type) do
+              xml.name(name)
+              xml.memory(memory_size)
+
+              if hugepages
+                xml.memoryBacking do
+                  xml.hugepages
+                end
+              end
+
+              xml.vcpu(cpus)
+              xml.os do
+                xml.type(os_type, :arch => arch)
+
+                boot_order.each do |dev|
+                  xml.boot(:dev => dev)
+                end
+              end
+              xml.features do
+                xml.acpi
+                xml.apic
+                xml.pae
+              end
+
+              unless cpu.empty?
+                if cpu[:mode]
+                  xml.cpu(:mode => cpu[:mode])
+                else
+                  xml.cpu do
+                    xml.model(cpu.dig(:model, :name), :fallback => cpu.dig(:model, :fallback) || "allow")
+                  end
+                end
+              end
+
+              xml.clock(:offset => "utc")
+
+              xml.devices do
+                ceph_args = read_ceph_args
+
+                volumes.each_with_index do |volume, index|
+                  target_device = "vd#{('a'..'z').to_a[index]}"
+                  if ceph_args && volume.pool_name.include?(ceph_args["libvirt_ceph_pool"])
+                    xml.disk(:type => "network", :device => "disk") do
+                      xml.driver(:name => "qemu", :type => volume.format_type, :cache => "writeback", :discard => "unmap")
+                      xml.source(:protocol => "rbd", :name => volume.path)
+
+                      ceph_args["monitor"]&.split(",")&.each do |monitor|
+                        xml.host(:name => monitor, :port => ceph_args["port"])
+                      end
+
+                      xml.auth(:username => ceph_args["auth_username"]) do
+                        if ceph_args.key?("auth_uuid")
+                          xml.secret(:type => "ceph", :uuid => ceph_args["auth_uuid"])
+                        else
+                          xml.secret(:type => "ceph", :usage => ceph_args["auth_usage"])
+                        end
+                      end
+
+                      xml.target(:dev => target_device, :bus => args["bus_type"] == "virtio" ? "virtio" : "scsi")
+                    end
+                  else
+                    xml.disk(:type => "file", :device => "disk") do
+                      xml.driver(:name => "qemu", :type => volume.format_type)
+                      xml.source(:file => volume.path)
+                      xml.target(:dev => target_device, :bus => "virtio")
+                    end
+                  end
+                end
+
+                if iso_file
+                  xml.disk(:type => "file", :device => "cdrom") do
+                    xml.driver(:name => "qemu", :type => "raw")
+                    xml.source(:file => "#{iso_dir}/#{iso_file}")
+                    xml.target(:dev => "hdc", :bus => "ide")
+                    xml.readonly
+                    xml.address(:type => "drive", :controller => 0, :bus => 1, :unit => 0)
+                  end
+                end
+
+                nics.each do |nic|
+                  xml.interface(:type => nic.type) do
+                    if nic.type == "bridge"
+                      xml.source(:bridge => nic.bridge)
+                    else
+                      xml.source(:network => nic.network)
+                    end
+                    xml.model(:type => nic.model)
+                  end
+                end
+
+                if guest_agent
+                  xml.channel(:type => "unix") do
+                    xml.target(:type => "virtio", :name => "org.qemu.guest_agent.0")
+                  end
+                end
+
+                xml.rng(:model => "virtio") do
+                  xml.backend(virtio_rng[:backend_path], :model => virtio_rng.fetch(:backend_model, "random"))
+                end
+
+                if arch == "s390x"
+                  xml.controller(:type => "scsi", :index => "0", :model => "virtio-scsi")
+                  xml.console(:type => "pty") do
+                    xml.target(:type => "sclp")
+                  end
+                  xml.memballoon(:model => "virtio")
+                else
+                  xml.serial(:type => "pty") do
+                    xml.target(:port => 0)
+                  end
+                  xml.console(:type => "pty") do
+                    xml.target(:port => 0)
+                  end
+                  xml.input(:type => "tablet", :bus => "usb")
+                  xml.input(:type => "mouse", :bus => "ps2")
+
+                  graphics = xml.graphics(:type => display[:type])
+                  if display[:port].empty?
+                    graphics.port = display[:port]
+                    graphics.autoport = "no"
+                  else
+                    graphics.port = -1
+                    graphics.autoport = "yes"
+                  end
+                  graphics.listen = display[:listen] unless display[:listen].empty?
+                  graphics.password = display[:password] unless display[:password].empty?
+
+                  xml.video do
+                    xml.model(:type => "cirrus", :vram => 9216, :heads => 1)
+                  end
+                end
+              end
+            end
+          end
+
+          builder.to_xml
+        end
+        # rubocop:enable Metrics
+
         private
         attr_accessor :volumes_path
+
+        def read_ceph_args(path = "/etc/foreman/ceph.conf")
+          return unless File.file?(path)
+
+          args = {}
+
+          File.readlines(path).each do |line|
+            pair = line.strip.split("=")
+            key = pair[0]
+            value = pair[1]
+            args[key] = value
+          end
+
+          args
+        end
 
         # This tests the library version before redefining the address
         # method for this instance to use a method compatible with

lib/fog/libvirt/models/compute/templates/server.xml.erb
@@ -1,155 +1,0 @@
-<domain type='<%= domain_type %>'>
-  <name><%= name %></name>
-  <memory><%= memory_size %></memory>
-<% if hugepages -%>
-  <memoryBacking>
-    <hugepages/>
-  </memoryBacking>
-<% end -%>
-  <vcpu><%= cpus %></vcpu>
-  <os>
-    <type arch='<%= arch %>'><%= os_type %></type>
-<% boot_order.each do |dev| -%>
-    <boot dev='<%= dev %>'/>
-<% end -%>
-  </os>
-  <features>
-    <acpi/>
-    <apic/>
-    <pae/>
-  </features>
-<% if !cpu.empty? -%>
-<% if cpu[:mode] -%>
-  <cpu mode='<%= cpu[:mode] %>'>
-<% else -%>
-  <cpu>
-<% end -%>
-<%
-  fallback = 'allow'
-  model = ''
-  if cpu[:model]
-    fallback = cpu[:model][:fallback] if cpu[:model][:fallback]
-    model    = cpu[:model][:name]     if cpu[:model][:name]
-  end
--%>
-    <model fallback='<%= fallback %>'><%= model %></model>
-  </cpu>
-<% end -%>
-  <clock offset='utc'/>
-  <devices>
-<% args = {}
-   if File.file?('/etc/foreman/ceph.conf')
-     File.readlines('/etc/foreman/ceph.conf').each do |line|
-       pair = line.strip.split("=")
-       key = pair[0]
-       value = pair[1]
-       args[key] = value
-     end
-   end
-%>
-<% volumes.each do |vol| -%>
-  <% if File.file?('/etc/foreman/ceph.conf') && vol.pool_name.include?(args["libvirt_ceph_pool"]) %>
-    <disk type='network' device='disk'>
-      <driver name='qemu' type='<%= vol.format_type %>' cache='writeback' discard='unmap'/>
-      <source protocol='rbd' name='<%= vol.path %>'>
-      <% if args.key?("monitor") -%>
-        <% args["monitor"].split(",").each do |mon| %>
-        <host name='<%= mon %>' port='<%= args["port"] %>'/>
-        <% end %>
-      <% end %>
-      </source>
-      <auth username='<%= args["auth_username"] %>'>
-       <% if args.key?("auth_uuid") -%>
-        <secret type='ceph' uuid='<%= args["auth_uuid"] %>'/>
-       <% else -%>
-        <secret type='ceph' usage='<%= args["auth_usage"] %>'/>
-       <% end -%>
-      </auth>
-      <% if args['bus_type'] == 'virtio' %>
-        <target dev='vd<%= ('a'..'z').to_a[volumes.index(vol)] %>' bus='virtio'/>
-      <% else %>
-        <target dev='sd<%= ('a'..'z').to_a[volumes.index(vol)] %>' bus='scsi'/>
-      <% end %>
-    </disk>
-  <% else %>
-    <disk type='file' device='disk'>
-      <driver name='qemu' type='<%= vol.format_type %>'/>
-      <source file='<%= vol.path %>'/>
-      <%# we need to ensure a unique target dev -%>
-      <target dev='vd<%= ('a'..'z').to_a[volumes.index(vol)] %>' bus='virtio'/>
-    </disk>
-  <% end %>
-<% end -%>
-<% if iso_file -%>
-    <disk type='file' device='cdrom'>
-      <driver name='qemu' type='raw'/>
-      <source file='<%= "#{iso_dir}/#{iso_file}" %>'/>
-      <target dev='hdc' bus='ide'/>
-      <readonly/>
-      <address type='drive' controller='0' bus='1' unit='0'/>
-    </disk>
-<% end -%>
-<% nics.each do |nic| -%>
-    <interface type='<%= nic.type %>'>
-      <source <%= nic.type == 'bridge' ? "bridge='#{nic.bridge}'" : "network='#{nic.network}'" %> />
-      <model type='<%= nic.model %>'/>
-    </interface>
-<% end -%>
-<% if guest_agent -%>
-    <channel type='unix'>
-      <target type='virtio' name='org.qemu.guest_agent.0'/>
-    </channel>
-<% end -%>
-    <rng model='virtio'>
-<%
-  rng_backend_model = virtio_rng[:backend_model] ? virtio_rng[:backend_model] : 'random'
--%>
-<% if virtio_rng[:backend_path] -%>
-      <backend model='<%= rng_backend_model %>'><%= virtio_rng[:backend_path] %></backend>
-<% else -%>
-      <backend model='<%= rng_backend_model %>'/>
-<% end -%>
-    </rng>
-<% if arch == "s390x" -%>
-    <controller type="scsi" index="0" model="virtio-scsi"/>
-    <console type="pty">
-      <target type="sclp"/>
-    </console>
-    <memballoon model="virtio"/>
-<% else -%>
-    <serial type='pty'>
-      <target port='0'/>
-    </serial>
-    <console type='pty'>
-      <target port='0'/>
-    </console>
-    <input type='tablet' bus='usb'/>
-    <input type='mouse' bus='ps2'/>
-<% end -%>
-<%
-display_type = display[:type]
-
-if display[:port].empty?
-  display_port = display[:port]
-  autoport = 'no'
-else
-  display_port = '-1'
-  autoport = 'yes'
-end
-
-unless display[:listen].empty?
-  display_listen = "listen='#{display[:listen]}'"
-end
-
-unless display[:password].empty?
-  display_password = "passwd='#{display[:password]}'"
-end
--%>
-<% if arch != "s390x" -%>
-    <graphics type='<%= display_type %>' port='<%= display_port %>' autoport='<%= autoport %>' <%= display_listen %> <%= display_password %> />
-    <video>
-      <model type='cirrus' vram='9216' heads='1'/>
-    </video>
-<% end -%>
-  </devices>
-</domain>

tests/libvirt/models/compute/server_tests.rb
@@ -59,5 +59,8 @@ Shindo.tests('Fog::Compute[:libvirt] | server model', ['libvirt']) do
       end
     end
     test('be a kind of Fog::Libvirt::Compute::Server') { server.kind_of? Fog::Libvirt::Compute::Server }
+    tests("serializes to xml") do
+      test("with memory") { server.to_xml.match?(%r{<memory>\d+</memory>}) }
+    end
   end
 end

commit 35e1be8245ecd9ff7d1cb14f13e214bf6bb1f737

Use correct ceph_args variable
Fixes: 6db6f8089418599044be39373f3baf17402b5cca ("Replace server XML with Nokogiri::XML::Builder")

lib/fog/libvirt/models/compute/server.rb
@@ -363,7 +363,7 @@ module Fog
                         end
                       end
 
-                      xml.target(:dev => target_device, :bus => args["bus_type"] == "virtio" ? "virtio" : "scsi")
+                      xml.target(:dev => target_device, :bus => ceph_args["bus_type"] == "virtio" ? "virtio" : "scsi")
                     end
                   else
                     is_block = volume.path.start_with?("/dev/")