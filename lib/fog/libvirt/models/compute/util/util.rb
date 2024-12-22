fog/fog-libvirt

commit 476c6305ebaa25dc12bea275f8583aaf6312aa90

Always use XML builders to construct XML
This removes the use of ERB to construct XML and instead always uses a
builder.

lib/fog/libvirt/models/compute/util/util.rb
@@ -16,14 +16,6 @@ module Fog
         attribute.nil? ? (xml/path).map : (xml/path).map{|element| element[attribute.to_sym]}
       end
 
-      def to_xml template_name = nil
-        # figure out our ERB template filename
-        erb = template_name || self.class.to_s.split("::").last.downcase
-        path     = File.join(File.dirname(__FILE__), "..", "templates", "#{erb}.xml.erb")
-        template = File.read(path)
-        ERB.new(template, nil, '-').result(binding)
-      end
-
       def randomized_name
         "fog-#{(SecureRandom.random_number*10E14).to_i.round}"
       end

lib/fog/libvirt/models/compute/network.rb
@@ -27,6 +27,17 @@ module Fog
         def shutdown
           service.destroy_network(uuid)
         end
+
+        def to_xml
+          builder = Nokogiri::XML::Builder.new do |xml|
+            xml.network do
+              xml.name(name)
+              xml.bridge(:name => bridge_name, :stp => 'on', :delay => '0')
+            end
+          end
+
+          builder.to_xml
+        end
       end
     end
   end

lib/fog/libvirt/models/compute/pool.rb
@@ -78,6 +78,20 @@ module Fog
         def volumes
           service.list_pool_volumes uuid
         end
+
+        def to_xml
+          builder = Nokogiri::XML::Builder.new do |xml|
+            xml.pool(:type => 'dir') do
+              xml.name(name)
+
+              xml.target do
+                xml.path(path)
+              end
+            end
+          end
+
+          builder.to_xml
+        end
       end
     end
   end

lib/fog/libvirt/models/compute/templates/network.xml.erb
@@ -1,6 +1,0 @@
-<network>
-  <name><%= name %></name>
-  <forward mode='<%= network_mode %>'/>
-  <bridge name='<%= bridge_name %>' stp='on' delay='0' />
-  </ip>
-</network>

lib/fog/libvirt/models/compute/templates/volume.xml.erb
@@ -1,34 +1,0 @@
-<volume>
-        <name><%= name %></name>
-        <allocation unit="<%= split_size_unit(allocation)[1] %>"><%= split_size_unit(allocation)[0] %></allocation>
-        <capacity unit="<%= split_size_unit(capacity)[1] %>"><%= split_size_unit(capacity)[0] %></capacity>
-        <target>
-          <format type="<%= format_type %>"/>
-          <permissions>
-            <% if owner -%>
-            <owner><%= owner %></owner>
-            <% end -%>
-            <% if group -%>
-            <group><%= group %></group>
-            <% end -%>
-            <mode>0744</mode>
-            <label>virt_image_t</label>
-          </permissions>
-        </target>
-        <% if backing_volume -%>
-        <backingStore>
-          <path><%= backing_volume.path %></path>
-          <format type="<%= backing_volume.format_type %>"/>
-          <permissions>
-            <% if owner -%>
-            <owner><%= owner %></owner>
-            <% end -%>
-            <% if group -%>
-            <group><%= group %></group>
-            <% end -%>
-            <mode>0744</mode>
-            <label>virt_image_t</label>
-          </permissions>
-        </backingStore>
-        <% end -%>
-</volume>

lib/fog/libvirt/models/compute/volume.rb
@@ -79,6 +79,47 @@ module Fog
           service.upload_volume(pool_name, name, file_path)
         end
 
+        def to_xml
+          builder = Nokogiri::XML::Builder.new do |xml|
+            xml.volume do
+              xml.name(name)
+
+              allocation_size, allocation_unit = split_size_unit(allocation)
+              xml.allocation(allocation_size, :unit => allocation_unit)
+
+              capacity_size, capacity_unit = split_size_unit(capacity)
+              xml.capacity(capacity_size, :unit => capacity_unit)
+
+              xml.target do
+                xml.format(:type => format_type)
+
+                xml.permissions do
+                  xml.owner(owner) if owner
+                  xml.group(group) if group
+                  xml.mode('0744')
+                  xml.label('virt_image_t')
+                end
+              end
+
+              if backing_volume
+                xml.backingStore do
+                  xml.path(backing_volume.path)
+                  xml.format(:type => backing_volume.format_type)
+
+                  xml.permissions do
+                    xml.owner(owner) if owner
+                    xml.group(group) if group
+                    xml.mode('0744')
+                    xml.label('virt_image_t')
+                  end
+                end
+              end
+            end
+          end
+
+          builder.to_xml
+        end
+
         private
 
         def image_suffix

tests/libvirt/models/compute/network_tests.rb
@@ -30,12 +30,14 @@ Shindo.tests('Fog::Compute[:libvirt] | network model', ['libvirt']) do
 
   tests("to_xml") do
     test("default") do
-      begin
-        network.to_xml
-        false
-      rescue NameError # forward_mode is undefined
-        true
-      end
+      expected = <<~NETWORK
+        <?xml version="1.0"?>
+        <network>
+          <name>default</name>
+          <bridge name="virbr0" stp="on" delay="0"/>
+        </network>
+      NETWORK
+      network.to_xml == expected
     end
   end
 end

tests/libvirt/models/compute/volume_tests.rb
@@ -38,18 +38,19 @@ Shindo.tests('Fog::Compute[:libvirt] | volume model', ['libvirt']) do
   test('to_xml') do
     test('default') do
       expected = <<~VOLUME
+        <?xml version="1.0"?>
         <volume>
-                <name>fog_test</name>
-                <allocation unit="G">1</allocation>
-                <capacity unit="G">10</capacity>
-                <target>
-                  <format type="raw"/>
-                  <permissions>
-                                            <mode>0744</mode>
-                    <label>virt_image_t</label>
-                  </permissions>
-                </target>
-                </volume>
+          <name>fog_test</name>
+          <allocation unit="G">1</allocation>
+          <capacity unit="G">10</capacity>
+          <target>
+            <format type="raw"/>
+            <permissions>
+              <mode>0744</mode>
+              <label>virt_image_t</label>
+            </permissions>
+          </target>
+        </volume>
       VOLUME
       volume.to_xml == expected
     end

commit 9ac702901129fe0ca95f234625ec4cb64155088d

Remove unused erb require
Fixes: 476c6305ebaa25dc12bea275f8583aaf6312aa90 ("Always use XML builders to construct XML")

lib/fog/libvirt/models/compute/util/util.rb
@@ -1,5 +1,4 @@
 require 'nokogiri'
-require 'erb'
 require 'securerandom'
 
 module Fog