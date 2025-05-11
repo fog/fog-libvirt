fog/fog-libvirt

commit 0277e9b42589836134763dd182fe6025bf8349df

Correctly load volumes after creation and cloning

lib/fog/libvirt/models/compute/volume.rb
@@ -39,7 +39,8 @@ module Fog
 
           raise Fog::Errors::Error.new('Reserving an existing volume may create a duplicate') if key
           @xml ||= to_xml
-          self.path = service.create_volume(pool_name, xml).path
+          self.id = service.create_volume(pool_name, xml).key
+          reload
         end
 
         # Destroy a volume
@@ -68,8 +69,8 @@ module Fog
           new_volume      = self.dup
           new_volume.key  = nil
           new_volume.name = new_name
-          new_volume.path = service.clone_volume(pool_name, new_volume.to_xml, self.name).path
 
+          new_volume.id = service.clone_volume(pool_name, new_volume.to_xml, self.name).key
           new_volume.reload
         end
 

commit 1614014d24c2ee7a198c24024f700fcde9bb5d65

Deal with missing volume_type
LVM volumes do not have a volume type and after a reload the value is
set to nil. 0277e9b42589836134763dd182fe6025bf8349df introduced that
reload after saving. The mock driver doesn't properly show this
behavior, which is why it was missed. A test is added to simulate the
real world behavior.

Fixes: 0277e9b42589836134763dd182fe6025bf8349df ("Correctly load volumes after creation and cloning")
Link: https://libvirt.org/formatstorage.html#storage-volume-target-elements
Link: https://libvirt.org/storage.html#valid-logical-volume-format-types

lib/fog/libvirt/models/compute/server.rb
@@ -368,7 +368,9 @@ module Fog
                   else
                     is_block = volume.path.start_with?("/dev/")
                     xml.disk(:type => is_block ? "block" : "file", :device => "disk") do
-                      xml.driver(:name => "qemu", :type => volume.format_type)
+                      driver = xml.driver(:name => "qemu")
+                      driver[:type] = volume.format_type if volume.format_type
+
                       if is_block
                         xml.source(:dev => volume.path)
                       else

lib/fog/libvirt/models/compute/volume.rb
@@ -91,7 +91,7 @@ module Fog
               xml.capacity(capacity_size, :unit => capacity_unit)
 
               xml.target do
-                xml.format(:type => format_type)
+                xml.format(:type => format_type) if format_type
                 xml_permissions(xml)
               end
 

tests/libvirt/models/compute/server_tests.rb
@@ -129,5 +129,39 @@ Shindo.tests('Fog::Compute[:libvirt] | server model', ['libvirt']) do
 
       os_firmware && secure_boot && enrolled_keys && loader_attributes
     end
+
+    test("with volumes") do
+      pool = Fog::Compute[:libvirt].pools.create(
+        :persistent => true,
+        :xml => <<~XML
+          <pool type='logical'>
+            <name>lvm-pool</name>
+            <source>
+              <name>vg_storage01</name>
+              <format type='lvm2'/>
+            </source>
+            <target>
+              <path>/dev/vg_storage01</path>
+            </target>
+          </pool>
+        XML
+      )
+      server = Fog::Compute[:libvirt].servers.new(
+        :nics => [],
+        :volumes => [
+          {
+            :path => "/dev/vg_storage01/volume01",
+            :pool_name => pool.name
+          }
+        ]
+      )
+      server.volumes.each do |volume|
+        volume.save
+        # mock driver doesn't simulate the real thing
+        # LVM doesn't have a volume type
+        volume.format_type = nil
+      end
+      !server.save.nil?
+    end
   end
 end